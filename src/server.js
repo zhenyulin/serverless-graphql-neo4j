3ad895c011001d2e55d7aae760f8f4fe692514a7
Zhenyu Lin <zhenyu.lyn@gmail.com>
2018-08-17 16:32:00 +0100

    config update

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ import { v1 as neo4j } from 'neo4j-driver';
 
 import schema from './graphql/schema';
 
-console.log(process.env);
-
 const driver = neo4j.driver(
 	process.env.NEO4J_URI,
 	neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD),
